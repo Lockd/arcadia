b6b7e2cd5f1292b398fa9fdacca2a270aae32bca
Lockd <pikagrigoriy@gmail.com>
2018-02-19 02:15:33 +0300

    checkbox1
    added story 2 and now users can simultaneously type text and get translation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os, msvcrt
 
 from translator import *
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QApplication
 
 class MyWin(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -10,20 +12,15 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        
-        if self.ui.checkBox.isChecked():
-            self.ui.textEdit.setText('первый чек')
-        else:
-            self.ui.pushButton.clicked.connect(self.Translate)
-            pass        
+        self.ui.pushButton.clicked.connect(self.Translate)
         self.ui.pushButton_2.clicked.connect(self.Clear)
+        self.ui.checkBox.stateChanged.connect(self.CheckLine)
+        
+        #if self.ui.checkBox.isChecked():            
+            #self.ui.textEdit.textChanged.connect(self.Translate)
+       
 
-    while 1:
-        a = msvcrt.getch()
-        os.system("cls")
-        print(a)
-
-    
+            
     #UI fuctions
     def Clear(self):
         self.ui.textEdit.setText("")
@@ -32,13 +29,32 @@ class MyWin(QtWidgets.QMainWindow):
     def Translate(self):
         if self.ui.checkBox_2.isChecked():
             self.ui.textEdit.setText('второй чек')
-            pass
         else:
             self.ui.textEdit_2.setText("")
             Result = self.ui.textEdit.toPlainText()
             self.ui.textEdit_2.setText(Result)
 
-       
+    def Translate_check1(self):
+        if self.ui.checkBox.isChecked():
+            self.ui.textEdit_2.setText("")
+            Result = self.ui.textEdit.toPlainText()
+            self.ui.textEdit_2.setText(Result)
+        else:
+            self.ui.textEdit_2.setText("")
+
+        
+    def CheckLine(self):
+        if self.ui.checkBox.isChecked():
+            self.ui.pushButton.setEnabled(0)
+            Result = self.ui.textEdit.toPlainText()
+            self.ui.textEdit_2.setText(Result)            
+            self.ui.textEdit.textChanged.connect(self.Translate_check1)
+        else:
+            self.ui.pushButton.setEnabled(1)
+
+    
+        
+        
 
 if __name__=="__main__":
     app = QtWidgets.QApplication(sys.argv)
